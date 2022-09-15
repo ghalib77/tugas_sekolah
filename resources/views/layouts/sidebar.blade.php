ed190d827c305a5491b422e49a83a3d75338493c
ghalib77 <muhamadghalib7@gmail.com>
2022-09-15 12:13:02 +0700

    feat: app layout

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,27 +8,15 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Pages</li>
-            <li class="active">
-                <a href="/home" class="nav-link "><i class="fas fa-bicycle"></i><span>Dashboard</span></a>
+            <li class="@if (Route::current()->uri == '/biodata') active @endif">
+                <a class="nav-link" href="/biodata"><i class='fab fa-phoenix-squadron'></i><span>Biodata</span></a>
             </li>
-            <li><a class="nav-link" href="/kelas"><i class="far fa-square"></i><span>Kelas</span></a></li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i>
-                    <span>Biodata</span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="/biodata">Data</a></li>
-                    <li><a href="/createpost">Create</a></li>
-                </ul>
+            <li class="@if (Route::current()->uri == '/kelas') active @endif">
+                <a class="nav-link" href="/kelas"><i class='fab fa-phoenix-squadron'></i><span>Kelas</span></a>
             </li>
-            <li class="menu-header">Component</li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i>
-                    <span>Utilities</span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="utilities-contact.html">Contact</a></li>
-                    <li><a class="nav-link" href="utilities-invoice.html">Invoice</a></li>
-                    <li><a href="utilities-subscribe.html">Subscribe</a></li>
-                </ul>
+            <li class="@if (Route::current()->uri == '/register/data') active @endif">
+                <a class="nav-link" href="/register/data"><i class='fab fa-phoenix-squadron'></i><span>Register
+                        Data</span></a>
             </li>
         </ul>
     </aside>
