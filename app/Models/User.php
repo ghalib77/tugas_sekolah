b1be825d6b755f062867b820f559f023776bd093
ghalib77 <muhamadghalib7@gmail.com>
2022-09-15 12:09:47 +0700

    feat: add User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        "email_verified_at"
     ];
 
     /**
@@ -35,12 +36,7 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function biodata(){
+        return $this->hasOne(Biodata::class, 'id', 'email_id');
+    }
 }
