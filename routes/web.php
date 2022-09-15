777e6a62096d724cb07b9b4e8e11f4eca4e06529
ghalib77 <muhamadghalib7@gmail.com>
2022-09-15 12:06:32 +0700

    feat: add route for another page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BiodataController;
 use App\Http\Controllers\KelasController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RegController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileController;
+use App\Models\Biodata;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,29 +20,58 @@ use App\Http\Controllers\KelasController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::redirect("/", "/login", 301);
 
-Route::get('/', function () {
-    return redirect("/auth");
+Route::get("/logout",function(){
+    Auth::logout();
+    return redirect("/login");
 });
 
-Route::get("/biodata",[BiodataController::class, "getBiodata"]);
+Route::middleware(["guest"])->group(function(){
 
-Route::get("/auth", function(){
-    return view("pages.auth");
+    Route::get("/login",[LoginController::class,"index"])->name("login");
+    Route::post("/loginCheck",[LoginController::class,"authenticate"]);
+
+    Route::prefix("/register")->group(function(){
+        Route::get("/",[RegController::class, "createIndex"]);
+        Route::post("/add",[RegController::class, "add"]);
+    });
+
 });
 
-Route::get("/home",function(){
-    return view("pages.main");
+Route::middleware(["auth"])->group(function(){
+
+    Route::prefix("/register")->group(function(){
+        Route::get("/data",[RegController::class, "index"]);
+        Route::get("/datatables",[RegController::class, "datatables"]);
+        Route::get("/edit/{query}",[RegController::class, "editIndex"]);
+        Route::put("/update/{query}", [RegController::class, "update"]);
+        Route::post("/delete", [RegController::class, "delete"]);
+    });
+
+    Route::prefix("/biodata")->group(function(){
+        Route::get("/", [BiodataController::class, "index"])->name("home");
+        Route::get("/datatables", [BiodataController::class, "datatables"]);
+        Route::get("/create", [BiodataController::class, 'addIndex']);
+        Route::get("/edit/{query}", [BiodataController::class, 'editIndex']);
+        Route::post("/store", [BiodataController::class, "store"]);
+        Route::post("/delete",[BiodataController::class, 'delete']);
+        Route::post('/update/{id}', [BiodataController::class, 'update']);
+    });
+
+    Route::prefix("/kelas")->group(function(){
+        Route::get("/", [KelasController::class, "index"]);
+        Route::get("/datatables", [KelasController::class, "datatables"]);
+        Route::get("/create", [KelasController::class, 'addIndex']);
+        Route::get("/edit/{query}", [KelasController::class, 'editIndex']);
+        Route::post("/store", [KelasController::class, "store"]);
+        Route::post("/delete",[KelasController::class, 'delete']);
+        Route::post('/update/{id}', [KelasController::class, 'update']);
+    });
+
+    Route::get('/profile', [ProfileController::class, 'index']);
+        
 });
 
-Route::get("/createpost", function(){
-   return view("pages.tambahBiodata");
-});
 
-Route::get("/kelas",[KelasController::class, "getAllKelas"]);
 
-Route::get("/kelasDetail/{query}",[KelasController::class, "getKelasDetail"]);
-
-Route::get("/biodata/{query}", [BiodataController::class, "getBiodataDetail"]);
-
-Route::post("/simpanBiodata", [BiodataController::class, "saveBiodata"]);
