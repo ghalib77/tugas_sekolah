d442e3e57b0e23528b62c78f7ebb87a254a094fd
ghalib77 <muhamadghalib7@gmail.com>
2022-09-15 12:06:46 +0700

    feat: add Biodata controller

--- app/Http/Controllers/BiodataController.php
+++ app/Http/Controllers/BiodataController.php
@@ -2,60 +2,125 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Biodata;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use App\Models\Kelas;
 use Illuminate\Support\Facades\Session;
+use Yajra\Datatables\Facades\Datatables;
+
+use App\Models\Biodata;
+use App\Models\Kelas;
 
 class BiodataController extends Controller
 {
 
-    public function saveBiodata(Request $request){
-       $request->validate([
+   public function index(){
+      return view("pages.biodata.index");
+   }
+
+   public function addIndex(){
+      return view("pages.biodata.create",[
+        "kelas"=>Kelas::all()
+      ]);
+   }
+
+   public function editIndex($query){
+    $data=Biodata::findOrFail($query);
+    return view("pages.biodata.edit",[
+      "query"=>$data,
+      "kelas"=>Kelas::all()
+    ]);
+   }
+
+   public function datatables(){
+    $query=Biodata::with(['kelas']);
+    return Datatables($query)
+          ->addColumn('action', function($query){
+            return "<div class='row'><button class='col-sm-12 mb-2 btn btn-sm btn-danger btn-delete-data' data-id='$query->id'>Hapus</button><a href='/biodata/edit/$query->id' class='btn btn-sm btn-primary btn-edit-data col-sm-12 gap-2' data-id='$query->id'>Edit</a></div>";
+          })
+          ->addIndexColumn()
+          ->rawColumns(['action'])
+          ->make();
+   }
+
+    public function delete(Request $req){
+      $id=$req->input('id');
+
+      try{
+          Biodata::destroy($id);
+          return response()->json(200);
+      }catch(\Throwable $th){
+          return abort(500, $th->getMessage());
+      }
+    }
+
+    public function store(Request $req){
+       $validatedData=$req->validate([
         "nama"=>"required|max:255",
-        "NIS"=>"required|unique:Biodata",
-        "kelas"=>"required",
+        "NIS"=>"required|unique:biodata",
+        "kelas_id"=>"required",
         "tempat_lahir"=>"required",
         "tanggal_lahir"=>"required",
-        "no_absen"=>"required"
+        "email"=>"required|unique:biodata"
        ],
        [
        'nama.required' => 'kolom nama harus diisi',
        'NIS.required' => 'kolom NIS harus diisi',
-       'kelas.required' => 'kolom kelas harus diisi',
+       'kelas_id.required' => 'kolom kelas harus diisi',
        'tempat_lahir.required' => 'kolom tempat lahir harus diisi',
        'tanggal_lahir.required' => 'kolom tanggal lahir harus diisi',
-       'no_absen.required' => 'kolom No absen lahir harus diisi',
+       'email.required'=> 'kolom email harus diisi',
+       'email.unique'=>'email sudah ada yang punya'
        ]
-       );
-       $kelas_id=collect(Kelas::all()->where("kelas", "=", $request->kelas))->first()->id;
+      );
 
-       $data=Biodata::create([
-        "nama"=>$request->nama,
-        "NIS"=>$request->NIS,
-        "kelas_id"=>$kelas_id,
-        "tempat_lahir"=>$request->tempat_lahir,
-        "tanggal_lahir"=>$request->tanggal_lahir,
-        "no_absen"=>$request->no_absen
-       ]);
-       if($data){
-          Session::flash("sukses", "Tambah data sukses");
-          return redirect("/createpost");
+        if($validatedData){
+          Biodata::create($validatedData);
+          return redirect("/biodata")->with("sukses", "Tambah data sukses");
        }
     }
 
-    public function getBiodata(){
-        return view("pages.biodata",[
-            "biodata"=>Biodata::latest()->filter(request(["search"]))->get()->load("kelas"),
-            "query"=>"biodata"
-          ]);
-    }
+    public function update(Request $req, $id){
+      $currentData=Biodata::findOrFail($id);
+      $validatedData=$req->validate([
+        "nama"=>"required|max:255",
+        "NIS"=>"required",
+        "kelas_id"=>"required",
+        "tempat_lahir"=>"required",
+        "tanggal_lahir"=>"required",
+        "email"=>"required"
+       ],
+       [
+       'nama.required' => 'kolom nama harus diisi',
+       'NIS.required' => 'kolom NIS harus diisi',
+       'kelas_id.required' => 'kolom kelas harus diisi',
+       'tempat_lahir.required' => 'kolom tempat lahir harus diisi',
+       'tanggal_lahir.required' => 'kolom tanggal lahir harus diisi',
+       'email.required'=> 'kolom email harus diisi',
+       ]
+      );
 
-    public function getBiodataDetail($query){
-        return view("pages.biodataDetail",[
-            "biodata"=>Biodata::with("kelas")->where("NIS", "=", $query)->firstOrFail()
-        ]);
+      
+      if($req->email !== $currentData->email){
+        $validatedData=$req->validate([
+          "email"=>"unique:biodata"
+         ],
+         [
+         'email.unique'=>'email sudah ada yang punya'
+         ]
+        ); 
+      }else if($req->NIS !== $currentData->NIS){
+        $validatedData=$req->validate([
+          "NIS"=>"unique:biodata",
+         ],
+         [
+         'NIS.unique"=>"NIS sudah ada yang punya',
+         ]
+        ); 
+      }
+
+      if($validatedData){
+          $currentData->update($validatedData);
+          return redirect("/biodata")->with("sukses", "Edit data sukses");
+      }
     }
 
 }
